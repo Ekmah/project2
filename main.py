b3b15e93c1368e9597f4e4a6d8b7367549436184
Thomas Poupinet <th.poupinet@free.fr>
2022-01-16 18:27:45 +0100

    added csv writing, gitignored the folder while adding makedirs to create folder if inexistant.

--- main.py
+++ main.py
@@ -4,32 +4,44 @@ import requests
 import time
 import re
 import csv
+import os
 
 
 def get_all_categories(link):
     response = requests.get(link)
     html = response.content
     soup = bs(html, "lxml")
-    categories = soup.find(class_='side_categories').find_all('a', href=True)
+    categories = soup.find(class_='side_categories').find('ul', recursive=False).find('ul').find_all('a', href=True)
     for categorie in categories:
         categorie_link = urljoin(link, categorie['href'])
         get_product_of_categorie(categorie_link)
 
 
-def get_product_of_categorie(category_url):
+def get_product_of_categorie(category_url, products=[]):
+    print(category_url)
     response = requests.get(category_url)
     html = response.content
     soup = bs(html, "lxml")
     links = soup.find_all('h3')
-    print(len(links))
     for link in links:
         link = urljoin(category_url, link.find('a', href=True)['href'])
-        # print(link)
-        # scrap_product(link)
+        products.append(scrap_product(link))
+
     next_page = soup.find(class_="next")
     if next_page:
         link = urljoin(category_url, next_page.find('a', href=True)['href'])
-        get_product_of_categorie(link)
+        get_product_of_categorie(link, products)
+    else:
+        category = soup.find('h1').get_text().replace(" ", "_")
+        fieldnames = ["product_page_url", "universal_product_code", "title", "price_including_tax",
+                      "price_excluding_tax", "number_available", "product_description", "category", "review_rating",
+                      "image_url"]
+        filename = f'CSVs/{category}.csv'
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, 'a', encoding='UTF8', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(products)
 
 
 def scrap_product(product_page_url):
@@ -47,13 +59,19 @@ def scrap_product(product_page_url):
     price_including_tax = content['Price (incl. tax)']
     price_excluding_tax = content['Price (excl. tax)']
     number_available = int(''.join(filter(str.isdigit, number_available)))
-    product_description = soup.find("article").find("p", recursive=False).get_text()
+    product_description = soup.find("article").find("p", recursive=False)
+    if product_description:
+        product_description = product_description.get_text()
+    else:
+        product_description = 'No description'
     category = soup.select_one("a[href*=category\/books\/]").get_text()
     review_rating = soup.find('p', class_="star-rating")['class'][1]
     image_url = soup.find("img")["src"]
 
-    return (product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax,
-            number_available, product_description, category, review_rating, image_url)
+    return {"product_page_url": product_page_url, "universal_product_code": universal_product_code,
+            "title": title, "price_including_tax": price_including_tax, "price_excluding_tax": price_excluding_tax,
+            "number_available": number_available, "product_description": product_description, "category": category,
+            "review_rating": review_rating, "image_url": image_url}
 
 
 def get_table(soup):
@@ -67,7 +85,5 @@ def get_table(soup):
     return content
 
 
-get_all_categories("http://books.toscrape.com/index.html")
-
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
+if __name__ == '__main__':
+    get_all_categories("http://books.toscrape.com/index.html")
